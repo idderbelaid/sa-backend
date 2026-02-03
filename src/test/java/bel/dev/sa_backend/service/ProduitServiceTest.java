118e5913d444fd12391341d2491a55400aa2af79
idderbelaid <144224181+idderbelaid@users.noreply.github.com>
2026-02-03 09:48:22 +0100

    Add Pageable import to ProduitServiceTest

--- src/test/java/bel/dev/sa_backend/service/ProduitServiceTest.java
+++ src/test/java/bel/dev/sa_backend/service/ProduitServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
